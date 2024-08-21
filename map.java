0dee65c276a7e9bf8b65d8e00cc729191a26332c
SQY25 <123747259+SQY25@users.noreply.github.com>
2024-08-21 19:57:00 -0400

    Update map.java
    started implementing hard-coded 2-dimensional array map

--- map.java
+++ map.java
@@ -1,1 +1,30 @@
+public class map {
+    public char[20][20] map;
 
+
+    public map {
+       addwall(9, 0, 0);
+       addwall(4, 0, 10);
+       addwall(9, 0, 15);
+      
+       
+       
+    }
+    public static void addwall(int n, int row, int column, bool horizontal) {
+        if (horizontal == False)
+        for (int i = 0; i < n; i ++) {
+            map[row][column] = "x";
+            if (column < 19) {
+               column += 1;
+            }
+            else {
+               row += 1;
+               column = 0;
+            }
+        }
+      return;
+    }
+    public String toString() {
+    }
+
+}
