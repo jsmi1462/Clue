af9d71ce101c3f49606578e9387401c62f635814
SQY25 <123747259+SQY25@users.noreply.github.com>
2024-08-26 16:51:37 -0400

    test
    lorem ipsum

--- map.java
+++ map.java
@@ -1,12 +1,42 @@
+import java.util.*;
+import java.io.*;
+
 public class map {
     public char[25][23] map;
+    public ArrayList<Player> players;
+    public HashMap<int[2], Room> doors;
 
 
-    public map {
+    public map (int nplayers) throws IOException {
+        players = new ArrayList<Player>();
+        doors = new HashMap<int[2], Room>();
+        
         this.addrow("xxxxxxx xxxxxxxx x");
         this.addrow("");
-       
+        for (int i = 0; i < 6 - nplayers; i ++) {
+            Player temp = new NPC();
+            players.add(temp);
+        }
+        // add player controlled players with name input
+        for (int i = 0; i < nplayers; i ++) {
+             boolean stop = false;
+             while (stop == false) {
+             System.out.println("Enter name for player" + i + ":");
+             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+             String playername = input.readLine();
+             System.out.println("Confirm player name is " + playername + ": (Y/N)"); //make sure the player name is correct
+             BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+             String response = input.readLine();
+             if (response == "Y") {
+             Player temp = new Player(playername);
+             players.add(temp);
+             stop == True
+             }
+             }
+        }
     }
+    
+    
     public void addrow(String row, int rowIndex) {
         int counter = 0;
         for (char c: row.toCharArray()) {
