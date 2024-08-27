7405a37591765d87c1b61f8c6a7fa4b2b6d1351f
olire1819 <olivia.resnick25@paceacademy.org>
2024-08-27 10:00:28 -0400

    Add files via upload

--- Room.java
+++ Room.java
@@ -1,3 +1,199 @@
-public class Room {
-    
+public class Room
+{
+    public String name;
+
+    public Room(String name)
+    {
+        this.name = name;
+    }
+
+    public void printRoomArt(String weaponName)
+    {
+        switch(weaponName)
+        {
+            case "candlestick":
+            {
+                System.out.println("                                 ...                               ");
+                System.out.println("                          .#%%#*:**%%%@                            ");
+                System.out.println("                         .%#%@%%%%@.:#%%                           ");
+                System.out.println("                         .+######%%%#*%#                           ");
+                System.out.println("                          .-*++#%%###+-.                           ");
+                System.out.println("                           .-#+##*+*#.                             ");
+                System.out.println("                            -***##**#.                             ");
+                System.out.println("                            :#*###***.                             ");
+                System.out.println("                            .-#+*#*#-.                             ");
+                System.out.println("                             :%@##%%.                              ");
+                System.out.println("                             .#+*#*#.                              ");
+                System.out.println("                             .#+###-                               ");
+                System.out.println("                              -=*##.                               ");
+                System.out.println("                              :####.                               ");
+                System.out.println("                              .####.                               ");
+                System.out.println("                              .##*#                                ");
+                System.out.println("                              .%###                                ");
+                System.out.println("                              .#%##.                               ");
+                System.out.println("                              :#%##.                               ");
+                System.out.println("                              %#%##:                               ");
+                System.out.println("                             .#*%#*#                               ");
+                System.out.println("                             .#*#%#*.                              ");
+                System.out.println("                            .%*#%#*##                             ");
+                System.out.println("                          ..##*#%***##.                            ");
+                System.out.println("                        +.-*%%@@@@*##*#**.                         ");
+                System.out.println("                      .########%%%#*#%**%##                        ");
+                System.out.println("                      :%%+*###%@@%#*#%%#%%#:                       ");
+                System.out.println("                     .:#%%*:..%@%%#+*+###@#.                     ");
+                System.out.println("                      .=#%%**.......+**#%%#.                       ");
+                System.out.println("                       .:==###@@%@%#####=-.                        ");
+                System.out.println("                          ..:=========-.                           ");
+                break;
+            }
+            case "wrench":
+            {
+                System.out.println("                                   .....         ");
+                System.out.println("                               ..@@@@@:.         ");
+                System.out.println("                             ..@@@@@-.           ");
+                System.out.println("                             .@@@@@.    . %*.    ");
+                System.out.println("                             .@@@@@*    *@@@     ");
+                System.out.println("                             .@@@@@@@@@@@@@:.    ");
+                System.out.println("                            -@@@@@@@@@@@@@.      ");
+                System.out.println("                          :@@@@@@@@@@@@@:.       ");
+                System.out.println("                        .@@@@@@@@-. .  .         ");
+                System.out.println("                      .@@@@@@@@+. .              ");
+                System.out.println("                    .@@@@@@@@%. .                ");
+                System.out.println("                  .%@@@@@@@@.                    ");
+                System.out.println("               ..*@@@@@@@@.                      ");
+                System.out.println("        ...   .=@@@@@@@@..                       ");
+                System.out.println("       .:@@@@@@@@@@@@@.                          ");
+                System.out.println("     ..@@@@@@@@@@@@@-.                           ");
+                System.out.println("     +@@@@@@@@@@@@%.                             ");
+                System.out.println("    .@@@+    @@@@@@.                             ");
+                System.out.println("    .%#.    ..@@@@@.                             ");
+                System.out.println("           .=@@@@@.                              ");
+                System.out.println("        ..:@@@@@.                                ");
+                break;
+            }
+            case "lead pipe":
+            {
+                System.out.println("                                               @%.       ");
+                System.out.println("                                             @@#@%@.     ");
+                System.out.println("                                              =+%#%%%.   ");
+                System.out.println("                                               .#%**@%.  ");
+                System.out.println("                                              . .@#****  ");
+                System.out.println("                                              %%@+=+**   ");
+                System.out.println("                                            .@@@%+%.     ");
+                System.out.println("                                          .##+@@#..      ");
+                System.out.println("                                        .#@+-*#          ");
+                System.out.println("                                     ..##*=%#            ");
+                System.out.println("                                    :##+-%*              ");
+                System.out.println("                                  -%#+-**                ");
+                System.out.println("                                +##*-#*                  ");
+                System.out.println("                              +@%*=#*                    ");
+                System.out.println("                           .*%#==%*                      ");
+                System.out.println("                          *#@-=**                        ");
+                System.out.println("                        *%%-=%*                          ");
+                System.out.println("                      *##=*@+                            ");
+                System.out.println("                    ##%=*%=                              ");
+                System.out.println("                  #+%=+%-                                ");
+                System.out.println("                ###=+%.                                  ");
+                System.out.println("              #%%+%%.                                    ");
+                System.out.println("            ###*#@.                                      ");
+                System.out.println("         .*%#=*%                                         ");
+                System.out.println("       .*%#*%%.                                          ");
+                System.out.println("     .#%#*%%.                                            ");
+                System.out.println("    %@#*%%.                                              ");
+                System.out.println("  +@%*%@.                                                ");
+                System.out.println("   %@@.                                                  ");
+                break;
+                                                                                                                
+            }
+            case "rope":
+            {
+                System.out.println("                                                :@@@@@@@@..                 ");
+                System.out.println("                                            .@@@@@@@@@@@@@@@@.              ");
+                System.out.println("                                          .@@@@@@*.    .#@@@@@@.            ");
+                System.out.println("                                         @@@@@:..%@@@@@@@ .#@@@@:           ");
+                System.out.println("                                        @@@@% *@@@@@@@@@@@@  @@@@.          ");
+                System.out.println("                                        @@@@..@@@@@:    -@@@@-.@@@@          ");
+                System.out.println("                                       @@@@..@@@@         @@@@..@@@+         ");
+                System.out.println("                         =@@          -@@@* @@@@           @@@-.@@@@         ");
+                System.out.println("                         @@@@      .@@@@@@.@@@@           @@@@..@@@@         ");
+                System.out.println("                         @@@@      @@@@@@ .@@@%          *@@@@ @@@@.         ");
+                System.out.println("                         @@@@   @@@.%@@@@@@@@@.        .@@@@@ -@@@@          ");
+                System.out.println("                         .@@@@..@@@@+.@@@@@@@@     .@@@@@@@. @@@@@           ");
+                System.out.println("                         .@@@@+ @@@@@..@@@@@@@@@@@@@@@@@..@@@@@#            ");
+                System.out.println("                           .@@@@@..@@@@@..@@@@@@@@@@% ..@@@@@@@.             ");
+                System.out.println("                         @@@..@@@@@ .@@@@@..@@@@@..=@@@@@@@@-.               ");
+                System.out.println("                         @@@@@..@@@@@.-@@@@@.+@@@@@@@@@@@..                  ");
+                System.out.println("                     @@@@.-@@@@@.+@@@@@.%@@@@%.@@@@@+.                       ");
+                System.out.println("                    .#@@@@@.%@@@@%.@@@@@# @@@@@-.@@@                         ");
+                System.out.println("                .@@@@@@@@@@@+ @@@@@-.@@@@@-.@@@@@                            ");
+                System.out.println("             -@@@@@@@@=..@@@@@..@@@@@. @@@@@..@@@                            ");
+                System.out.println("         . @@@@@@@. .@@@@@@@@@@@. @@@@@..@@@@@                               ");
+                System.out.println("         %@@@@@ .@@@@@@@@@@@@@@@@@..@@@@@.-@@.                               ");
+                System.out.println("        @@@@@..@@@@@@@. .   @@@@@@@@.*@@@@.                                  ");
+                System.out.println("       @@@@= @@@@@.         @@@@@@@@@%.@@=.                                  ");
+                System.out.println("      .@@@@ @@@@*          %@@@..@@@@@@                                      ");
+                System.out.println("      @@@@..@@@%.         .@@@@ @@@@@%.                                      ");
+                System.out.println("      @@@@ -@@@.         .@@@@.*@@@:                                         ");
+                System.out.println("     .+@@@. @@@@        .@@@@ :@@@@.                                         ");
+                System.out.println("       @@@@ -@@@@-.   :@@@@@..@@@@.                                          ");
+                System.out.println("       .@@@@. @@@@@@@@@@@@*.%@@@@.                                           ");
+                System.out.println("       .:@@@@*..@@@@@@@%. :@@@@@                                             ");
+                System.out.println("          @@@@@@%..   .*@@@@@@.                                              ");
+                System.out.println("            @@@@@@@@@@@@@@@@                                                 ");
+                System.out.println("              ..@@@@@@@@..                                                   ");
+                break;
+            }
+            case "dagger":
+            {
+                System.out.println("                                           :**+++.");
+                System.out.println("                                           *#*++**");
+                System.out.println("                                       . .%@%*+**+");
+                System.out.println("                                       .=@%@@%#+..");
+                System.out.println("                                    ...@@@@%@.    ");
+                System.out.println("                                    .@@@@@@...    ");
+                System.out.println("                         -+:.. ....@@@@%%.        ");
+                System.out.println("                         .*+*:...%@%@%%....       ");
+                System.out.println("                         ..*+=#%%%%%%-.           ");
+                System.out.println("                         :++**:=%@%*...           ");
+                System.out.println("                       .+:---.#*=+*...            ");
+                System.out.println("                     .*+=-:.-**+#*+++..           ");
+                System.out.println("                   .*++=-.:+*++++..+++.           ");
+                System.out.println("                  ++=+=-.-***++.                  ");
+                System.out.println("                :**++=:+#***+...                  ");
+                System.out.println("             ..***++=*##***:                      ");
+                System.out.println("             **+*+++####*=.                       ");
+                System.out.println("          .#**+*++###**+.                         ");
+                System.out.println("         =*****#*#*##*.                           ");
+                System.out.println("       .#***+#*#####.                             ");
+                System.out.println("     .#*#**##%####..                              ");
+                System.out.println("    :##**#*%####:.                                ");
+                System.out.println("  .###*##%####:. .                                ");
+                System.out.println(". ##*#%%%%##-.                                    ");
+                System.out.println(" ###%%%%%#.. .                                    ");
+                System.out.println("+#%%#%#+..                                        ");
+                System.out.println("%%%*...                                           ");
+                break;
+            }
+            case "revolver":
+            {
+                System.out.println("                     =-@@@@@@@@@@@@@                      .%@@@@.     ");
+                System.out.println("                   +%%%%=@@@@@@@@@*%%%+%%%%%%%%%%%%%%%%%%%%%%%%%@     ");
+                System.out.println("                    @%%@@%%%@#####*%%%+%%%%%%%%%%%%%%%%%%%%%%%%%@     ");
+                System.out.println("              ..:%%%%%%%%%%@-%%%%%=%%%+%%%%%%%%%%%%%%%%%%@%@%@.       ");
+                System.out.println("            @%%@+%%%%%@+@%%%%%%%%%*%%%+++==++@@@@@@@@@@@@+#=*         ");
+                System.out.println("            @%%%%%-%%%%%%===+=====%%%=                                ");
+                System.out.println("           #%%.:@%%%@%%%%%%@=*@%%%%%%.                                ");
+                System.out.println("          @%%%%%%%%%%@@+.#%      @                                    ");
+                System.out.println("        :%%%%*@%%%%%%%-   @      +                                    ");
+                System.out.println("       @%%%%%%%%%%.   .-   :+  @:                                     ");
+                System.out.println("      +%%%%%%%%%         :.                                           ");
+                System.out.println("      @%%%%%%%%%.                                                     ");
+                System.out.println("      #%%%%%%%%%%.                                                    ");
+                System.out.println("     #%%%%%%%%%%#                                                     ");
+                System.out.println("    @%%%%%%%%%%%%                                                     ");
+                System.out.println("     @@%%%%%@@@                                                       ");
+                break;
+            }
+        }
+    }
 }
