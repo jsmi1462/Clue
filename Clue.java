8862d2a29c5f5a68df4b9e3f2cb61e0c39599a24
SQY25 <123747259+SQY25@users.noreply.github.com>
2024-09-30 08:56:02 -0400

    merge conflict :DDD

--- Clue.java
+++ Clue.java
@@ -5,18 +5,14 @@ public class Clue {
 
 
     public static void main(String[] args) {
-        map gamemap = new map(6);
+        map gamemap = new map(0);
         while (gamemap.gameover == false) {
             for (int i = 0; i < gamemap.players.size(); i ++) {
                 int roll = 1 + (int) (Math.random() * 6);
                 int roll2 = 1 + (int) (Math.random() * 6);
                 int roll3 = roll + roll2;
-                    for (int j = roll3; j > 0 ; j--) {
-                        System.out.println("Player " + gamemap.players.get(i) + " (" + i + ") is playing with " + j + " moves remaining this turn!");
-                        System.out.println(gamemap);
-                        //System.out.println("Player " + i + "is currently in space " + gamemap.players.get(i).xPos + " , " + gamemap.players.get(i).yPos);
-                        if (!gamemap.moveplayer(i)) break;
-                }
+                //System.out.println("Player " + i + "is currently in space " + gamemap.players.get(i).xPos + " , " + gamemap.players.get(i).yPos);
+                gamemap.moveplayer(i, roll3);
             }
         }
     }
