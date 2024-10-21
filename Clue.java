48e116a9d7c56c98607fc3ab7ff2dba86616436e
SQY25 <123747259+SQY25@users.noreply.github.com>
2024-10-21 11:05:46 -0400

    Update Clue.java

--- Clue.java
+++ Clue.java
@@ -2,18 +2,27 @@ import java.util.*;
 
 public class Clue {
     public ArrayList<String> answer;
-
-
     public static void main(String[] args) {
-        map gamemap = new map(2);
-        while (gamemap.gameover == false) {
-            for (int i = 0; i < gamemap.players.size(); i ++) {
+        map gameMap = new map(1);
+        int winner = 0;
+        while (gameMap.gameOver == false) {
+            int limit = gameMap.playersingame.size();
+            for (int i = 0; i < limit; i ++) {
                 int roll = 1 + (int) (Math.random() * 6);
                 int roll2 = 1 + (int) (Math.random() * 6);
                 int roll3 = roll + roll2;
                 //System.out.println("Player " + i + "is currently in space " + gamemap.players.get(i).xPos + " , " + gamemap.players.get(i).yPos);
-                gamemap.moveplayer(i, roll3);
+                if (gameMap.moveplayer(i, roll3) == false) {
+                    i--;
+                    limit--;
+                }
+                if (gameMap.gameOver == true) {
+                    winner = i;
+                    break;
+                }
             }
         }
+        gameMap.win(gameMap.players.get(winner));
     }
+
 }
